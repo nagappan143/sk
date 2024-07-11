73f17cee2d000f5c18b83ea077b1d752530e1a16
nagappan143 <131938719+nagappan143@users.noreply.github.com>
2024-07-11 12:36:29 +0530

    Add files via upload

---
+++ SLIDE NAVBAR/js/main.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+function hidesidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+
+}
